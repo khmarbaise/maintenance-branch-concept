58fa6c8654fa9dfeaefdd6ca174d874a54657789
Karl Heinz Marbaise <info@soebes.de>
2012-02-12 12:49:29 +0100

    - First state.

---
+++ src/test/java/com/soebes/regelung/raumverwaltung/RaumBelegungTest.java
@@ -0,0 +1,95 @@
+package com.soebes.regelung.raumverwaltung;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.soebes.regelung.raumverwaltung.BelegungsZeitraum;
+import com.soebes.regelung.raumverwaltung.BelegungsZeitraumAnfangGleichEndeException;
+import com.soebes.regelung.raumverwaltung.BelegungsZeitraumEndeVorAnfangException;
+import com.soebes.regelung.raumverwaltung.Raum;
+import com.soebes.regelung.raumverwaltung.RaumBelegung;
+
+@Test
+public class RaumBelegungTest extends TestBase {
+
+    public void nichtVerfuegbarTest()
+            throws BelegungsZeitraumEndeVorAnfangException,
+            BelegungsZeitraumAnfangGleichEndeException {
+        RaumBelegung rb = new RaumBelegung(new Raum("Raum 1"));
+        BelegungsZeitraum zeitraum1 = new BelegungsZeitraum(
+                parseDate("01.01.2009 12:00:00"),
+                parseDate("01.01.2009 15:00:00"));
+        rb.addBelegung(zeitraum1);
+
+        BelegungsZeitraum zeitraum2 = new BelegungsZeitraum(
+                parseDate("01.01.2009 12:00:00"),
+                parseDate("01.01.2009 15:00:00"));
+
+        Assert.assertEquals(rb.isVerfuegbar(zeitraum2), false);
+    }
+
+    public void isVerfuegbarTest()
+            throws BelegungsZeitraumEndeVorAnfangException,
+            BelegungsZeitraumAnfangGleichEndeException {
+
+        RaumBelegung rb = new RaumBelegung(new Raum("Raum 1"));
+        BelegungsZeitraum zeitraum1 = new BelegungsZeitraum(
+                parseDate("01.01.2009 12:00:00"),
+                parseDate("01.01.2009 15:00:00"));
+        rb.addBelegung(zeitraum1);
+
+        BelegungsZeitraum zeitraum2 = new BelegungsZeitraum(
+                parseDate("01.01.2009 16:00:00"),
+                parseDate("01.01.2009 17:00:00"));
+
+        Assert.assertEquals(rb.isVerfuegbar(zeitraum2), true);
+    }
+
+    public void verfuegbarMehrereZeitraeumeTest()
+            throws BelegungsZeitraumEndeVorAnfangException,
+            BelegungsZeitraumAnfangGleichEndeException {
+        RaumBelegung rb = new RaumBelegung(new Raum("Raum 1"));
+        BelegungsZeitraum zeitraum = new BelegungsZeitraum(
+                parseDate("01.01.2009 12:00:00"),
+                parseDate("01.01.2009 15:00:00"));
+        rb.addBelegung(zeitraum);
+
+        zeitraum = new BelegungsZeitraum(parseDate("01.01.2009 16:00:00"),
+                parseDate("01.01.2009 17:00:00"));
+        rb.addBelegung(zeitraum);
+
+        zeitraum = new BelegungsZeitraum(parseDate("01.01.2009 15:00:00"),
+                parseDate("01.01.2009 15:09:59"));
+        rb.addBelegung(zeitraum);
+
+        BelegungsZeitraum buchungsZeit = new BelegungsZeitraum(
+                parseDate("01.01.2009 15:10:00"),
+                parseDate("01.01.2009 15:30:00"));
+
+        Assert.assertEquals(rb.isVerfuegbar(buchungsZeit), true);
+    }
+
+    public void verfuegbarMehrereZeitraeumeIntersectTest()
+            throws BelegungsZeitraumEndeVorAnfangException,
+            BelegungsZeitraumAnfangGleichEndeException {
+        RaumBelegung rb = new RaumBelegung(new Raum("Raum 1"));
+        BelegungsZeitraum zeitraum = new BelegungsZeitraum(
+                parseDate("01.01.2009 12:00:00"),
+                parseDate("01.01.2009 14:30:00"));
+        rb.addBelegung(zeitraum);
+
+        zeitraum = new BelegungsZeitraum(parseDate("01.01.2009 15:00:00"),
+                parseDate("01.01.2009 15:09:59"));
+        rb.addBelegung(zeitraum);
+
+        zeitraum = new BelegungsZeitraum(parseDate("01.01.2009 16:00:00"),
+                parseDate("01.01.2009 17:00:00"));
+        rb.addBelegung(zeitraum);
+
+        BelegungsZeitraum buchungsZeit = new BelegungsZeitraum(
+                parseDate("01.01.2009 14:45:00"),
+                parseDate("01.01.2009 14:55:00"));
+        Assert.assertEquals(rb.isVerfuegbar(buchungsZeit), true);
+    }
+
+}
---
+++ src/test/java/com/soebes/regelung/raumverwaltung/ResourcesTest.java
@@ -0,0 +1,27 @@
+package com.soebes.regelung.raumverwaltung;
+
+import org.testng.annotations.Test;
+
+import com.soebes.regelung.raumverwaltung.BelegungsZeitraum;
+import com.soebes.regelung.raumverwaltung.Raum;
+import com.soebes.regelung.raumverwaltung.RaumBelegung;
+
+@Test
+public class ResourcesTest extends TestBase {
+
+    public void resourceTest() throws Exception {
+        Raum raum = new Raum("Raum 1");
+        RaumBelegung rb = new RaumBelegung(raum);
+
+        BelegungsZeitraum zeitraum = new BelegungsZeitraum(
+                parseDate("01.01.2009 12:00:00"),
+                parseDate("01.01.2009 15:00:00"));
+        rb.addBelegung(zeitraum);
+
+        // if (rb.isVerfuegbar(zeitraum)) {
+        // //rb.addBelegung ();
+        // } else {
+        // //Raum ist in dem Zeitraum nich mehr verf�gbar.
+        // }
+    }
+}
